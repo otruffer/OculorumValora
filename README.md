860ae2bd75286909dcec8f4b59f22203690dd70b
Oskar Truffer <ot@studer-raimann.ch>
2017-01-04 11:16:00 +0100

    works with radiotap header.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 pcap-ecample
 ------------
-we extract the thernet address of each packet.
+we extract the ethernet addresses of each packet. Only works if your driver is in monitor mode:
+
+```
+ifconfig wlan0 down
+iwconfig wlan0 mode Monitor
+ifconfig wlan0 up
+```
--- src/Lib.hs
+++ src/Lib.hs
@@ -11,30 +11,37 @@ import qualified Data.ByteString as B
 import Data.Char
 import qualified Data.Map as Map
 import Data.Maybe
+import Foreign
+import qualified Network.Pcap.Base as BPcap
+import Text.Bytedump as ByteDump
+import Data.Binary.Strict.Get
+import qualified Data.Binary.Get as GL
+import Data.Either.Unwrap
 
 -- List of all known mac addresses.
 macMap :: Map.Map String String
-macMap = Map.fromList [("8C705ABA1458", "Someone's Laptop")]
+macMap = Map.fromList [("48db50c0c6ce", "Natel")]
 
 -- main Method.
 scanAndWork :: IO ()
 scanAndWork = do
     putStrLn "Top of the morning to you!"
-    handle <- Pcap.openLive "wlan0" 0 False 2000
+    handle <- Pcap.openLive "wlan0" 100 True 2000 -- we monitor wlan0 adapter. 100 bytes should be enough to get the header.
     lists <- Pcap.listDatalinks handle  -- We output the possible operation data
-    mapM_ print lists                   -- links. should be ~wlan 802_11
-
-    forever $ do
-        threadDelay 1000                 -- We don't let the main thread die.
-        Pcap.dispatchBS handle 1 printPackageBS   -- We don't need the resulting number of packages read.
+    mapM_ print lists                   -- links. should be ~wlan 802_11.. or some ethernet link layer protocol.
+    Pcap.loopBS handle (-1) printPackageBS   -- We don't need the resulting number of packages read.
                                                         -- Furthermore we register the callback printPackage
                                                         -- This operation will start in a different thread.
+    forever $ do
+        threadDelay 1000                 -- We don't let the main thread die.
 
+-- helper function if using Pointer Interface of Network.Pcap
 printPackagePtr :: Pcap.PktHdr -> Ptr Word8 -> IO ()
 printPackagePtr header@(Pcap.PktHdr _ _ capturedLength wireLength) body = do
     (_, bodyBS) <- Pcap.toBS (header, body)
     printPackage capturedLength wireLength bodyBS
 
+-- helper function if using ByteString Interface of Network.Pcap
 printPackageBS :: Pcap.PktHdr -> B.ByteString -> IO ()
 printPackageBS (Pcap.PktHdr _ _ capturedLength wireLength) body = printPackage capturedLength wireLength body
 
@@ -42,14 +49,45 @@ printPackage :: Integral a => a -> a -> B.ByteString -> IO ()
 printPackage capturedLength wireLength body = do
     putStrLn $ "Package length (captured) " ++ (show $ fromIntegral capturedLength)
     putStrLn $ "Package length (on wire) " ++ (show $ fromIntegral wireLength)
-    putStrLn $ "ByteString Body length: " ++ (show $ B.length body)
-    putStrLn $ "Extracted Mac Address: " ++ (show $ extractMacFromBS body)
-    putStrLn $ macToName $ show $ B.unpack $ extractMacFromBS body
+    putStrLn $ "Head Dump of the package (in hex): " ++ (prettyHex $ ByteDump.dumpRaw $ B.unpack $ B.take 30 body)
+    putStrLn $ "RadioTab Header Length:  " ++ (show $ radioTapLength)
+    putStrLn $ "Extracted Dest Mac Address: " ++ (prettyHex destAsString)
+    putStrLn $ "Extracted Source Mac Address: " ++ (prettyHex sourceAsString)
+    putStrLn $ macToName sourceAsString
     putStrLn "--------------------------"
+    where
+        radioTapLength = extractRadioTapLength body
+        destAsString = ByteDump.dumpRaw $ B.unpack $ extractDestMacFromBS radioTapLength body
+        sourceAsString = ByteDump.dumpRaw $ B.unpack $ extractSourceMacFromBS radioTapLength body
 
--- Drops 14 bytes then takes 6 bytes from the packet. These 6 bytes represent the mac address
+prettyHex :: String -> String
+prettyHex [] = []
+prettyHex (x:y:[]) = [x, y]
+prettyHex (x:y:rest) = [x, y, ':'] ++ (prettyHex rest)
+
+-- We expect to be in monitor mode thus we have a variable length radiotap header before the ethernet layer.
+-- extractRadioTapLength :: B.ByteString -> Int
+-- extractRadioTapLength = fromIntegral . B.unpack . B.head . B.drop 2
+extractRadioTapLength :: B.ByteString -> Int
+extractRadioTapLength body = fromRight $ fst $ runGet radioTapParser body
+
+-- We expect to be in monitor mode thus we have a variable length radiotap header before the ethernet layer.
+radioTapParser :: Get Int
+radioTapParser = do
+    skip 2
+    radioTabLength <- getWord8
+    return (fromIntegral(radioTabLength) :: Int)
+
+-- Drops (radiotap header) + 4 (some ethernet stuff) + 6 (destination MAC) bytes then takes 6 bytes from the packet.
+-- These 6 bytes represent the source mac address
 -- Notice: This is the senders Mac address: https://en.wikipedia.org/wiki/Ethernet_frame
-extractMacFromBS = B.take 6 . B.drop 14
+extractSourceMacFromBS :: Int -> B.ByteString -> B.ByteString
+extractSourceMacFromBS radioTapLength = B.take 6 . B.drop (radioTapLength + 4 + 6)
+
+extractDestMacFromBS :: Int -> B.ByteString -> B.ByteString
+extractDestMacFromBS radioTapLength = B.take 6 . B.drop (radioTapLength + 4)
+
+
 
 -- We can map a mac address to a name.
 macToName :: String -> String
